428c809024e62931b1481c3f28b1368ab5cec2c5
Jon Keam <jpkeam@gmail.com>
2024-06-20 12:38:40 -0400

    add vm endpoint

--- api/main.py
+++ api/main.py
@@ -7,13 +7,14 @@ app = FastAPI()
 if environ.get("PYTHON_ENV", "PRODUCTION") == "PRODUCTION":
     config.load_incluster_config()
 
-def list_all_pods(v1):
+def list_all_pods():
+    v1 = client.CoreV1Api()
     print("Listing pods with their IPs:")
     ret = v1.list_pod_for_all_namespaces(watch=False)
     for i in ret.items:
         print(f"{i.status.pod_ip}\t{i.metadata.namespace}\t{i.metadata.name}")
 
-def get_pods(v1, namespace) -> list[dict[str, str]]:
+def get_pods(namespace:str) -> list[dict[str, str]]:
     v1 = client.CoreV1Api()
     pod_list = v1.list_namespaced_pod(namespace)
     return list(map(lambda pod: {
@@ -21,6 +22,20 @@ def get_pods(v1, namespace) -> list[dict[str, str]]:
         "phase": pod.status.phase,
         "ip": pod.status.pod_ip
     }, pod_list.items))
+
+def get_vms() -> list[dict[str, str]]:
+    instances = client.CustomObjectsApi().list_cluster_custom_object(group="kubevirt.io", version="v1", plural="virtualmachineinstances")
+
+    return list(map(lambda instance: {
+        "name": instance['metadata']['name']
+        "cpu": instance['spec']['domain']['cpu']['cores'],
+        "memory": instance['spec']['domain']['memory']['guest'],
+        "created_at": instance['metadata']['creationTimestamp'],
+        "os": instance['metadata']['annotations']['vm.kubevirt.io/os'],
+        "disks": instance['spec']['domain']['devices']['disks'],
+        "interfaces": instance['spec']['domain']['devices']['interfaces'],
+        "machine_type": instance['spec']['domain']['machine']['type'],
+    }, instances.items))
 
 
 @app.get("/")
@@ -33,5 +48,8 @@ def read_health():
 
 @app.get("/pods")
 def read_pods():
-    v1 = client.CoreV1Api()
-    return {"pods": get_pods(v1, 'jkeam')}
+    return {"pods": get_pods('jkeam')}
+
+@app.get("/vms")
+def read_pods():
+    return {"pods": get_vms()}
