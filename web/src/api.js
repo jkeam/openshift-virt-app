c52503e9fc0ae76ebaed6f3cfc3d9f4e5248c0e2
Jon Keam <jpkeam@gmail.com>
2024-06-20 12:54:17 -0400

    start of wiring

--- web/src/api.js
+++ web/src/api.js
@@ -1,7 +1,12 @@
-export const fetchInitialVMs = () => {
-  return [
-    { id: 1, name: 'one', os: 'two', cpus: 'three', memory: 'four', storage: 'five' },
-    { id: 2, name: 'one - 2', os: null, cpus: null, memory: 'four - 2', storage: 'five - 2' },
-    { id: 3, name: 'one - 3', os: 'two - 3', cpus: 'three - 3', memory: 'four - 3', storage: 'five - 3' }
-  ];
+import fetch from 'node-fetch';
+BASE_URL = 'ocp-virt-app-api';
+
+export const fetchInitialVMs = async() => {
+  const response = await fetch(`http://${BASE_URL}/vms`)
+  return await response.json()
+  // return [
+  //   { id: 1, name: 'one', os: 'two', cpus: 'three', memory: 'four', storage: 'five' },
+  //   { id: 2, name: 'one - 2', os: null, cpus: null, memory: 'four - 2', storage: 'five - 2' },
+  //   { id: 3, name: 'one - 3', os: 'two - 3', cpus: 'three - 3', memory: 'four - 3', storage: 'five - 3' }
+  // ];
 }
